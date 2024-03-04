ebc2f61892227fcc1e0275a3c63db93ec7dabe3d
sephhhh <stephenwu07curry@gmail.com>
2024-03-04 17:01:30 -0500

    added grid,snake starting point, and randomly generated location for apple

---
+++ main.py
@@ -0,0 +1,52 @@
+import pygame
+import random
+
+
+
+
+def main():
+    global screen
+    pygame.init()
+
+    screen = pygame.display.set_mode((900,900))
+    snake = pygame.Rect(450, 450, 50, 50)
+    point = pygame.Rect(randomWidth(), randomHeight(),50,50)
+    screen.fill("black")
+
+    while True:
+        drawGrid()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                raise SystemExit
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    pygame.quit()
+                elif event.key == pygame.K_RIGHT:
+                    snake = snake.move(50,0)
+                elif event.key == pygame.K_LEFT:
+                    snake = snake.move(-50,0)
+                elif event.key == pygame.K_DOWN:
+                    snake = snake.move(0,50)
+                elif event.key == pygame.K_UP:
+                    snake = snake.move(0,-50)
+
+            pygame.draw.rect(screen, "red", snake)
+            pygame.draw.rect(screen, "green", point)
+            pygame.display.flip()
+
+def randomWidth():
+    return random.randint(0,18)*50
+
+def randomHeight():
+    return random.randint(0,18)*50
+
+def drawGrid():
+    blockSize = 50
+    for x in range(0, 900, blockSize):
+        for y in range(0, 900, blockSize):
+            rect = pygame.Rect(x,y, blockSize, blockSize)
+            pygame.draw.rect(screen, "white", rect, 1)
+
+if __name__ == "__main__":
+    main()
